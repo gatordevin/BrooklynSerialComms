c9a1d241361919ff8d2d826f1e22b7f992d0f25f
Devin Willis <gatordevin@gmail.com>
2020-07-10 10:31:11 -0400

    Added ability to send and read packets
    memory gets allocated and read to a struct to allow for organized data. Timeout is also enabled in case not enough bytes are sent.

--- BrooklynSerialComms.cpp
+++ BrooklynSerialComms.cpp
@@ -8,11 +8,49 @@ BrooklynSerialComms::BrooklynSerialComms(int baud)
 void BrooklynSerialComms::begin()
 {
     Serial.begin(_baud);
-    Serial.setTimeout(250);
+    Serial.setTimeout(100);
+    while (!Serial) {}
 }
 
 void BrooklynSerialComms::send()
 {
     byte* byte_array = reinterpret_cast<byte*>(&send_packet);
-    Serial.write(byte_array, sizeof(send_packet));
+    if(Serial.availableForWrite()>sizeof(send_packet)){
+        Serial.write(byte_array, sizeof(send_packet));
+        Serial.flush();
+    }
+}
+
+int BrooklynSerialComms::check_for_conn(int handshake_byte)
+{
+    if(Serial.available()){
+        int msg = Serial.read();
+        if(msg == handshake_byte){
+            if(Serial.availableForWrite()){
+                Serial.write(msg);
+                Serial.flush();
+                return msg;
+            }
+        }else{
+            return msg;
+        }
+        
+    }else{
+        return 0;
+    }
+    
+}
+
+void BrooklynSerialComms::check_for_data(void(*on_data_callback)(packet recv_packet), void(*no_data_callback)()){
+    if(Serial.available()){
+        if(Serial.readBytes((char*)&recv_packet, sizeof(packet))==sizeof(packet)){ //DATA LENGTH CHECK
+            if(recv_packet.header!=255){
+                recv_packet.data = 160; //HEADER CHECK
+            }
+            on_data_callback(recv_packet);
+        }
+        
+    }else{
+        no_data_callback();
+    }
 }
--- BrooklynSerialComms.h
+++ BrooklynSerialComms.h
@@ -3,9 +3,13 @@
 struct packet
 {
     byte header = 255;
+
     byte data_len;
+
     byte data;
+
     byte stop = 120;
+    
 };
 
 class BrooklynSerialComms
@@ -14,7 +18,10 @@ class BrooklynSerialComms
 
     public:
         packet send_packet;
+        packet recv_packet;
         BrooklynSerialComms(int baud);
         void begin();
         void send();
+        int check_for_conn(int handshake_byte);
+        void check_for_data(void(*on_data_callback)(packet data), void(*no_data_callback)());
 };
