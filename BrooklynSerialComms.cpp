d55fb3a30f6322ec6fff30513973267b772dde33
Devin Willis <gatordevin@gmail.com>
2020-07-11 01:12:14 -0400

    Finish Comms Library
    Finished comms library fully functional. Data is received and put into the structure where it can then be read and used in the code. Need to clean up actual use of the code to match the python mirror library.

--- BrooklynSerialComms.cpp
+++ BrooklynSerialComms.cpp
@@ -1,27 +1,80 @@
 #include <BrooklynSerialComms.h>
 
-BrooklynSerialComms::BrooklynSerialComms(int baud)
+BrooklynSerialComms::BrooklynSerialComms(unsigned int baud, short watchdog_timer)
 {
     _baud = baud;
+    _watchdog_timer = watchdog_timer;
+    _packets_recieved = 0;
+    _header_length_errors = 0;
+    _checksum_errors = 0;
+    _data_length_errors = 0;
+    _watchdog_errors = 0;
 }
 
 void BrooklynSerialComms::begin()
 {
     Serial.begin(_baud);
     Serial.setTimeout(100);
-    while (!Serial) {}
 }
 
-void BrooklynSerialComms::send()
+void BrooklynSerialComms::close(void(*end_comms_handler)()){
+    _data_length_errors = 0;
+    _packets_recieved = 0;
+    _checksum_errors = 0;
+    _header_length_errors = 0;
+    end_comms_handler();
+}
+
+void BrooklynSerialComms::send_full_packet()
 {
     byte* byte_array = reinterpret_cast<byte*>(&send_packet);
-    if(Serial.availableForWrite()>sizeof(send_packet)){
-        Serial.write(byte_array, sizeof(send_packet));
+    if(Serial.availableForWrite()>sizeof(packet)-sizeof(send_packet.data)){
+        Serial.write(byte_array, sizeof(packet)-sizeof(send_packet.data)+send_packet.data_length);
         Serial.flush();
     }
 }
 
-int BrooklynSerialComms::check_for_conn(int handshake_byte)
+void BrooklynSerialComms::send_byte(byte value, byte destination_id){
+    send_packet.header = 255;
+    send_packet.destination_id = destination_id;
+    send_packet.sender_id = 1;
+    send_packet.packet_id = recv_packet.packet_id;
+    send_packet.command = 7;
+    send_packet.data_length = 1;
+    send_packet.checksum = 0;
+    send_packet.data[0] = value;
+    send_full_packet();
+
+}
+
+void BrooklynSerialComms::send_metrics(byte destination_id){
+    send_packet.header = 255;
+    send_packet.destination_id = destination_id;
+    send_packet.sender_id = 1;
+    send_packet.packet_id = recv_packet.packet_id;
+    // send_packet.packed_id_1 = recv_packet.packed_id_1;
+    // send_packet.packed_id_2 = recv_packet.packed_id_2;
+    // send_packet.packed_id_3 = recv_packet.packed_id_3;
+    // send_packet.packed_id_4 = recv_packet.packed_id_4;
+    send_packet.command = 9;
+    send_packet.data_length = 12;
+    send_packet.checksum = 0;
+    send_packet.data[0] = _packets_recieved;
+    send_packet.data[1] = (_packets_recieved >> 8) & 0xFF;
+    send_packet.data[2] = (_packets_recieved >> 16) & 0xFF;
+    send_packet.data[3] = (_packets_recieved >> 24) & 0xFF;
+    send_packet.data[4] = (byte) _header_length_errors;
+    send_packet.data[5] = (byte) _header_length_errors >> 8;
+    send_packet.data[6] = (byte) _checksum_errors;
+    send_packet.data[7] = (byte) _checksum_errors >> 8;
+    send_packet.data[8] = (byte) _data_length_errors;
+    send_packet.data[9] = (byte) _data_length_errors >> 8;
+    send_packet.data[10] = (byte) _watchdog_errors;
+    send_packet.data[11] = (byte) _watchdog_errors >> 8;
+    send_full_packet();
+}
+
+int BrooklynSerialComms::check_for_conn(unsigned short handshake_byte)
 {
     if(Serial.available()){
         int msg = Serial.read();
@@ -29,6 +82,7 @@ int BrooklynSerialComms::check_for_conn(int handshake_byte)
             if(Serial.availableForWrite()){
                 Serial.write(msg);
                 Serial.flush();
+                _ser_connect_time = millis();
                 return msg;
             }
         }else{
@@ -41,16 +95,40 @@ int BrooklynSerialComms::check_for_conn(int handshake_byte)
     
 }
 
-void BrooklynSerialComms::check_for_data(void(*on_data_callback)(packet recv_packet), void(*no_data_callback)()){
-    if(Serial.available()){
-        if(Serial.readBytes((char*)&recv_packet, sizeof(packet))==sizeof(packet)){ //DATA LENGTH CHECK
-            if(recv_packet.header!=255){
-                recv_packet.data = 160; //HEADER CHECK
+void BrooklynSerialComms::check_for_data(void(*on_data_callback)(packet recv_packet), void(*no_data_callback)(), void(*watchdog_handler)()){
+    _current_time = millis();
+    if(_current_time-_ser_connect_time<(_watchdog_timer*2) || _packets_recieved>0){
+        if(_current_time-_last_recv_time<_watchdog_timer || _packets_recieved==0){
+            if(Serial.available()){
+                _last_recv_time  = _current_time;
+                if(Serial.readBytes((char*)&recv_packet, sizeof(packet)-sizeof(recv_packet.data))==sizeof(packet)-sizeof(recv_packet.data)){
+                    if(Serial.readBytes((char*)&recv_packet.data, recv_packet.data_length)==recv_packet.data_length){
+                        _packets_recieved += 1;
+                        on_data_callback(recv_packet);
+                        no_data_callback();
+                    }else{
+                        _data_length_errors += 1;
+                    }
+                }else{
+                    _header_length_errors += 1;
+                }
+            }else{
+                no_data_callback();
             }
-            on_data_callback(recv_packet);
+        }else{
+            _data_length_errors = 0;
+            _packets_recieved = 0;
+            _checksum_errors = 0;
+            _header_length_errors = 0;
+            _watchdog_errors += 1;
+            watchdog_handler();
         }
-        
     }else{
-        no_data_callback();
+        _data_length_errors = 0;
+        _packets_recieved = 0;
+        _checksum_errors = 0;
+        _header_length_errors = 0;
+        _watchdog_errors += 1;
+        watchdog_handler();
     }
 }
--- BrooklynSerialComms.h
+++ BrooklynSerialComms.h
@@ -2,26 +2,48 @@
 
 struct packet
 {
-    byte header = 255;
-
-    byte data_len;
-
-    byte data;
-
-    byte stop = 120;
+    byte header;
     
+    byte destination_id;
+
+    byte sender_id;
+
+    unsigned long packet_id;
+
+    byte command;
+
+    byte data_length;
+
+    unsigned short checksum;
+
+    byte data[50];
 };
 
 class BrooklynSerialComms
 {
-    int _baud;
+    unsigned int _baud;
+    unsigned short _watchdog_timer;
+    unsigned short _ser_connect_time;
+    unsigned long _packets_recieved;
+    unsigned int _header_length_errors;
+    unsigned int _data_length_errors;
+    unsigned int _checksum_errors;
+    unsigned int _watchdog_errors;
 
     public:
-        packet send_packet;
-        packet recv_packet;
-        BrooklynSerialComms(int baud);
+        
+        BrooklynSerialComms(unsigned int baud, short watchdog_timer);
         void begin();
-        void send();
-        int check_for_conn(int handshake_byte);
-        void check_for_data(void(*on_data_callback)(packet data), void(*no_data_callback)());
+        void send_full_packet();
+        void send_byte(byte value, byte destination_id);
+        void send_metrics(byte destination_id);
+        void close(void(*end_comms_handler)());
+        int check_for_conn(unsigned short handshake_byte);
+        void check_for_data(void(*on_data_callback)(packet data), void(*no_data_callback)(), void(*watchdog_handler)());
+        packet recv_packet;
+        packet send_packet;
+        
+    private:
+        int _last_recv_time;
+        int _current_time;
 };
