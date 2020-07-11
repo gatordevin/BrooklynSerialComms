6763cd933238b7325304d4e6a6c9fbdb9dbd2959
Devin Willis <gatordevin@gmail.com>
2020-07-11 13:02:08 -0400

    Added support for readign variables

--- BrooklynSerialComms.cpp
+++ BrooklynSerialComms.cpp
@@ -9,12 +9,81 @@ BrooklynSerialComms::BrooklynSerialComms(unsigned int baud, short watchdog_timer
     _checksum_errors = 0;
     _data_length_errors = 0;
     _watchdog_errors = 0;
+    _starting_pos = 0;
 }
 
 void BrooklynSerialComms::begin()
 {
     Serial.begin(_baud);
     Serial.setTimeout(100);
+}
+
+float BrooklynSerialComms::read_float()
+{
+    four_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    four_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    four_byte_converter.b[2] = recv_packet.data[2+_starting_pos];
+    four_byte_converter.b[3] = recv_packet.data[3+_starting_pos];
+    _starting_pos += 4;
+    return four_byte_converter.fval;
+}
+
+unsigned int BrooklynSerialComms::read_unsigned_int(){
+    four_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    four_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    four_byte_converter.b[2] = recv_packet.data[2+_starting_pos];
+    four_byte_converter.b[3] = recv_packet.data[3+_starting_pos];
+    _starting_pos += 4;
+    return four_byte_converter.uival;
+}
+
+int BrooklynSerialComms::read_int(){
+    four_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    four_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    four_byte_converter.b[2] = recv_packet.data[2+_starting_pos];
+    four_byte_converter.b[3] = recv_packet.data[3+_starting_pos];
+    _starting_pos += 4;
+    return four_byte_converter.ival;
+}
+
+short BrooklynSerialComms::read_short(){
+    two_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    two_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    _starting_pos += 2;
+    return two_byte_converter.sval;
+}
+
+unsigned short BrooklynSerialComms::read_unsigned_short(){
+    two_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    two_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    _starting_pos += 2;
+    return two_byte_converter.usval;
+}
+
+long long BrooklynSerialComms::read_long_long(){
+    eight_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    eight_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    eight_byte_converter.b[2] = recv_packet.data[2+_starting_pos];
+    eight_byte_converter.b[3] = recv_packet.data[3+_starting_pos];
+    eight_byte_converter.b[4] = recv_packet.data[4+_starting_pos];
+    eight_byte_converter.b[5] = recv_packet.data[5+_starting_pos];
+    eight_byte_converter.b[6] = recv_packet.data[6+_starting_pos];
+    eight_byte_converter.b[7] = recv_packet.data[7+_starting_pos];
+    _starting_pos += 8;
+    return eight_byte_converter.lval;
+}
+
+unsigned long long BrooklynSerialComms::read_unsigned_long_long(){
+    eight_byte_converter.b[0] = recv_packet.data[0+_starting_pos];
+    eight_byte_converter.b[1] = recv_packet.data[1+_starting_pos];
+    eight_byte_converter.b[2] = recv_packet.data[2+_starting_pos];
+    eight_byte_converter.b[3] = recv_packet.data[3+_starting_pos];
+    eight_byte_converter.b[4] = recv_packet.data[4+_starting_pos];
+    eight_byte_converter.b[5] = recv_packet.data[5+_starting_pos];
+    eight_byte_converter.b[6] = recv_packet.data[6+_starting_pos];
+    eight_byte_converter.b[7] = recv_packet.data[7+_starting_pos];
+    _starting_pos += 8;
+    return eight_byte_converter.ulval;
 }
 
 void BrooklynSerialComms::close(void(*end_comms_handler)()){
@@ -52,10 +121,6 @@ void BrooklynSerialComms::send_metrics(byte destination_id){
     send_packet.destination_id = destination_id;
     send_packet.sender_id = 1;
     send_packet.packet_id = recv_packet.packet_id;
-    // send_packet.packed_id_1 = recv_packet.packed_id_1;
-    // send_packet.packed_id_2 = recv_packet.packed_id_2;
-    // send_packet.packed_id_3 = recv_packet.packed_id_3;
-    // send_packet.packed_id_4 = recv_packet.packed_id_4;
     send_packet.command = 9;
     send_packet.data_length = 12;
     send_packet.checksum = 0;
@@ -97,6 +162,7 @@ int BrooklynSerialComms::check_for_conn(unsigned short handshake_byte)
 
 void BrooklynSerialComms::check_for_data(void(*on_data_callback)(packet recv_packet), void(*no_data_callback)(), void(*watchdog_handler)()){
     _current_time = millis();
+    _starting_pos = 0;
     if(_current_time-_ser_connect_time<(_watchdog_timer*2) || _packets_recieved>0){
         if(_current_time-_last_recv_time<_watchdog_timer || _packets_recieved==0){
             if(Serial.available()){
--- BrooklynSerialComms.h
+++ BrooklynSerialComms.h
@@ -14,9 +14,28 @@ struct packet
 
     byte data_length;
 
-    unsigned short checksum;
+    signed long checksum;
 
     byte data[50];
+};
+
+union four_byte_conv {
+    byte b[4];
+    float fval;
+    unsigned int uival;
+    int ival;
+};
+
+union two_byte_conv {
+    byte b[2];
+    unsigned short int usval;
+    short int sval;
+};
+
+union eight_byte_conv {
+    byte b[8];
+    unsigned long long ulval;
+    long long lval;
 };
 
 class BrooklynSerialComms
@@ -29,6 +48,7 @@ class BrooklynSerialComms
     unsigned int _data_length_errors;
     unsigned int _checksum_errors;
     unsigned int _watchdog_errors;
+    unsigned short _starting_pos;
 
     public:
         
@@ -38,10 +58,20 @@ class BrooklynSerialComms
         void send_byte(byte value, byte destination_id);
         void send_metrics(byte destination_id);
         void close(void(*end_comms_handler)());
+        float read_float();
+        unsigned int read_unsigned_int();
+        int read_int();
+        unsigned short read_unsigned_short();
+        short read_short();
+        long long read_long_long();
+        unsigned long long read_unsigned_long_long();
         int check_for_conn(unsigned short handshake_byte);
         void check_for_data(void(*on_data_callback)(packet data), void(*no_data_callback)(), void(*watchdog_handler)());
         packet recv_packet;
         packet send_packet;
+        four_byte_conv four_byte_converter;
+        two_byte_conv two_byte_converter;
+        eight_byte_conv eight_byte_converter;
         
     private:
         int _last_recv_time;
